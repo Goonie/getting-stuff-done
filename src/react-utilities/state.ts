a8343e59961cae8d00fa93ff3bd7209ada145232
Paul Martens <paul.martens@divotion.com>
2021-06-05 16:39:41 +0200

    Updated order of properties

--- src/react-utilities/state.ts
+++ src/react-utilities/state.ts
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 function useErrorMessage() {
-  const [message, setMessage] = useState<string | null>(null);
   const [isError, setIsError] = useState(false);
+  const [message, setMessage] = useState<string | null>(null);
 
   function setErrorMessage(errorMessage: string) {
-    setMessage(errorMessage);
     setIsError(true);
+    setMessage(errorMessage);
   }
 
   function clearErrorMessage() {
-    setMessage(null);
     setIsError(false);
+    setMessage(null);
   }
 
-  return { errorMessage: message, isError, setErrorMessage, clearErrorMessage };
+  return { isError, errorMessage: message, setErrorMessage, clearErrorMessage };
 }
 
 export { useErrorMessage };
